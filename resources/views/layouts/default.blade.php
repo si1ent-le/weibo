aa3ac13de05500beb44779b0796caaa8685d18ed
si1ent-le <test09619@gmail.com>
2020-11-21 14:08:19 +0800

    style-update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,8 +4,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title','Web 应用') - Laravel学习</title>
+  <link rel="stylesheet" href="css/app.css"></link>
 </head>
 <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Web 应用</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
     @yield('content')
-</body>
+    </div>
+  </body>
 </html>
