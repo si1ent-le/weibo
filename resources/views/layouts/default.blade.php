8ceef2ff5e2a7c35e2424d79adf9e14c5ecb7464
si1ent-le <test09619@gmail.com>
2020-11-21 16:55:38 +0800

    命名路由

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,20 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title','Web 应用') - Laravel学习</title>
-  <link rel="stylesheet" href="css/app.css"></link>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}"></link>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="/">Web 应用</a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
-      </ul>
-    </div>
-  </nav>
+  @include('layouts._header')
   <div class="container">
-    @yield('content')
+     <div class="offset-md-1 col-md-8">
+        @yield('content')
+        @include('layouts._footer')
     </div>
+  </div>
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
   <div class="jumbotron">
     <h1>欢迎学习 Laravel</h1>
-    <p class="lead"><a href="www.si1ent.xyz">si1ent Blog</a></p>
+    <p class="lead"><a href="http://www.si1ent.xyz" target="_blank">si1ent Blog</a></p>
     <p>一切，从这里开始！</p>
     <p><a class="btn btn-lg btn-success" href="#" role="button">注册</a></p>
   </div>
