5a9dd0d52e5b5bfaa45188c0231e98f6518e4646
si1ent-le <test09619@gmail.com>
2020-12-01 21:06:23 +0800

    删除用户

---
+++ database/migrations/2020_12_01_122101_add_is_admin_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIsAdminToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //在用户表中指定字段类型并指定默认值为“false”
+
+            $table->boolean('is_admin')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('is_admin');
+        });
+    }
+}
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
             $user = User::find(1);
             $user->name = 'si1ent';
             $user->email = 'si1ent@qq.com';
+            $user->is_admin= true;
             $user->save();
         }
 }
