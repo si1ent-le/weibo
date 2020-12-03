da06066c216fff3a2190a9999aa7502e1a1baf53
si1ent-le <test09619@gmail.com>
2020-12-03 15:06:44 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,10 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    //指明一个用户拥有多个微博
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
